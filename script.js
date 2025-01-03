2e9519f996f0822faa3c5904f5525c9980c3a749
Danish Alfattah <168889537+danishalfattah1@users.noreply.github.com>
2025-01-03 21:36:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active')
+    navmenu.classList.toggle('hidden')
+});
+
+
+
